fe15f8fdcc910c69db17ae1469bc4e345f7a2ee0
Govind <you@example.com>
2024-05-31 11:31:37 +0530

    updating_Code

--- middleware/auth_token.js
+++ middleware/auth_token.js
@@ -3,18 +3,53 @@ const jwt = require("jsonwebtoken");
 const { prisma } = require("../DB/db.config");
 
 
+// module.exports = async (req, res, next) => {
+//     try {
+//         const authHeader = req.headers.authorization;
+//         const token_with = req.header('Authorization')
+//         if (!authHeader || !authHeader.startsWith('Bearer ') ||  !token_with) {
+//             return res.status(401).json({ msg: "Unauthorized - No token provided" });
+//         }
+
+//         const token = authHeader.split(' ')[1] || req.header('Authorization')
+//         const decoded = jwt.verify(token, process.env.SECRET);
+//         if (!decoded) {
+//             return res.status(401).json({ msg: "Invalid token" });
+//         }
+//         const user = await prisma.user.findFirst({ where: { id: decoded.userId } });
+//         const admin = await prisma.admin.findFirst({ where: { id: decoded.userId } });
+//         const entity = user || admin;
+
+//         if (!entity) {
+//             return res.status(404).json({ msg: "User not found" });
+//         }
+//         const { password, ...entityWithoutPassword } = entity;
+//         req.user = entityWithoutPassword;
+//         next();
+//     } catch (error) {
+//         console.error("Authentication error:", error);
+//         if (error.name === "TokenExpiredError") {
+//             res.status(401).json({ msg: "Unauthorized - Token has expired" });
+//         } else {
+//             res.status(500).json({ msg: "Internal Server Error" });
+//         }
+//     }
+// };
+
 module.exports = async (req, res, next) => {
     try {
-        const authHeader = req.headers.authorization;
-        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        const token = req.headers.authorization || req.header('Authorization');
+        
+        if (!token) {
             return res.status(401).json({ msg: "Unauthorized - No token provided" });
         }
 
-        const token = authHeader.split(' ')[1];
         const decoded = jwt.verify(token, process.env.SECRET);
+        
         if (!decoded) {
             return res.status(401).json({ msg: "Invalid token" });
         }
+
         const user = await prisma.user.findFirst({ where: { id: decoded.userId } });
         const admin = await prisma.admin.findFirst({ where: { id: decoded.userId } });
         const entity = user || admin;
@@ -22,6 +57,7 @@ module.exports = async (req, res, next) => {
         if (!entity) {
             return res.status(404).json({ msg: "User not found" });
         }
+
         const { password, ...entityWithoutPassword } = entity;
         req.user = entityWithoutPassword;
         next();
@@ -34,3 +70,4 @@ module.exports = async (req, res, next) => {
         }
     }
 };
+
